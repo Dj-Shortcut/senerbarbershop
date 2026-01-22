d472ee3fec33943b7cdebf212031eca033360c93
Andy Arijs <shortcutcomputerguy@gmail.com>
2026-01-22 14:28:20 +0100

    SEO: favicon and layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Michroma } from "next/font/google";
+import Script from "next/script";
 
 const michroma = Michroma({
   subsets: ["latin"],
@@ -22,6 +23,49 @@ export default function RootLayout({
           overflow-x-hidden
         `}
       >
+        {/* JSON-LD Structured Data for Google */}
+        <Script
+          id="barber-jsonld"
+          type="application/ld+json"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BarberShop",
+              name: "The Barber Sener",
+              image: "https://thesenerbarber.shop/og-image.jpg",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "Beverstraat 22",
+                addressLocality: "Ninove",
+                postalCode: "9400",
+                addressCountry: "BE"
+              },
+              telephone: "+32488383871",
+              priceRange: "€€",
+              openingHoursSpecification: [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: [
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday"
+                  ],
+                  opens: "09:00",
+                  closes: "19:00"
+                }
+              ],
+              aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.9",
+                reviewCount: "120"
+              }
+            })
+          }}
+        />
+
         {children}
       </body>
     </html>
